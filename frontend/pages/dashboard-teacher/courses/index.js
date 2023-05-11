e43e63f513fab93815fd5a864b009627f91a8d1b
Hassan Ali <hassanali.bscsf19@iba-suk.edu.pk>
2023-05-11 20:39:34 +0500

    code refactored to solve routing problem

--- frontend/pages/dashboard-teacher/courses/index.js
+++ frontend/pages/dashboard-teacher/courses/index.js
@@ -5,8 +5,10 @@ import axios from "axios";
 import Cookies from "js-cookie";
 
 const Index = () => {
+
     const [courses, setCourses] = useState([]);
     const [loading, setLoading] = useState(false);
+
     useEffect(() => {
         if (courses.length === 0) {
             getCourses().then((res) => {
@@ -25,56 +27,63 @@ const Index = () => {
         <LayoutTeacher>
 
 
-        {/*=============== Start of main ================= */}
-        <main>
-            <h1>Overview</h1>
+            {/*=============== Start of main ================= */}
+            <main>
+                <h1>Overview</h1>
 
-            {/* ============= Start of Courses ================= */}
-            <div className="courses-table">
+                {/* ============= Start of Courses ================= */}
+                <div className="courses-table">
 
-                <h2>Courses</h2>
+                    <h2>Courses</h2>
 
-                <table>
-                    <thead>
-                    <tr>
-                        <th>Course Title</th>
-                        <th>Lessons</th>
-                        <th>Details</th>
-                    </tr>
-                    </thead>
+                    <table>
+                        <thead>
+                        <tr>
+                            <th>Course Title</th>
+                            <th>Lessons</th>
+                            <th>Details</th>
+                        </tr>
+                        </thead>
 
-                    <tbody>
-                    {loading ? courses.map((res, index) => {
-                        const course = res.data.course;
-                        return (<tr key={index}>
-                            <td>{course.name}</td>
-                            {course.lessons ? <td>{course.lessons.length}</td> : <td>0</td>}
-                            <td>
-                                <Link href={`/dashboard-teacher/courses/${course._id}`}>
-                                    click
-                                </Link>
-                            </td>
-                        </tr>)
-                    }) : ""
-                    }
-                    </tbody>
-                </table>
+                        <tbody>
+                        {loading ? courses.map((res, index) => {
+                            const course = res.data.course;
+                            return (<tr key={index}>
+                                <td>{course.name}</td>
+                                {course.lessons ? <td>{course.lessons.length}</td> : <td>0</td>}
+                                <td>
+                                    <Link
+                                        // href={`/dashboard-teacher/courses/${course._id}`}
+                                        href={{
+                                            pathname: `/dashboard-teacher/courses/course`,
+                                            query: {
+                                                courseId: course._id,
+                                                courseName: course.name
+                                            }
+                                        }}
+                                    >
+                                        click
+                                    </Link>
+                                </td>
+                            </tr>)
+                        }) : ""
+                        }
+                        </tbody>
+                    </table>
 
-            </div>
-            {/* ============= End of Courses  ================== */}
+                </div>
+                {/* ============= End of Courses  ================== */}
 
-        </main>
-        {/*=============== End Of Main  ==================*/}
+            </main>
+            {/*=============== End Of Main  ==================*/}
 
 
-    </LayoutTeacher>
+        </LayoutTeacher>
 
     );
 };
 
 export default Index;
-
-
 
 
 const getCourses = async () => {
