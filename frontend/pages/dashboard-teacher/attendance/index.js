c66199b2bb401865b6ed7b4bc7a8147dc614274e
Hassan Ali <hassanali.bscsf19@iba-suk.edu.pk>
2023-05-11 22:32:20 +0500

    updated

--- frontend/pages/dashboard-teacher/attendance/index.js
+++ frontend/pages/dashboard-teacher/attendance/index.js
@@ -1,48 +1,79 @@
-import React from "react";
+import React, {useEffect} from "react";
 import LayoutTeacher from "../../../components/Dashboard/Layout/LayoutTeacher";
 import Link from "next/link";
-import {} from "./attendanceController";
+import {getCourses} from "./attendanceController";
 
 
 const Attendance = () => {
-  return (
-    <LayoutTeacher>
-      {/*=============== Start of main ================= */}
-      <main>
-        <h1>Attendance</h1>
+    const [courses, setCourses] = React.useState([]);
+    const [loading, setLoading] = React.useState(false);
 
-        {/* ============= Start of Courses ================= */}
-        <div className="courses-table">
-          <h2>Attendance</h2>
 
-          <table>
-            <thead>
-              <tr>
-                <th>Serial No.</th>
-                <th>Course Name</th>
-                <th>View Details</th>
-              </tr>
-            </thead>
+    useEffect(() => {
+        getCourses().then(r => setCourses(r.courses));
+    }, []);
 
-            <tbody>
-              <tr>
-                <td>01</td>
-                <td>English</td>
-                <Link
-                  href={"/dashboard-teacher/attendance/attendance-details"}
-                  className="primary"
-                >
-                  Open
-                </Link>
-              </tr>
-            </tbody>
-          </table>
-        </div>
-        {/* ============= End of Courses  ================== */}
-      </main>
-      {/*=============== End Of Main  ==================*/}
-    </LayoutTeacher>
-  );
+    useEffect(() => {
+        if (loading === false && courses.length !== 0) {
+            setLoading(true);
+            console.log(courses);
+        }
+    }, [courses]);
+
+    return (
+        <LayoutTeacher>
+            {/*=============== Start of main ================= */}
+            <main>
+                <h1>Attendance</h1>
+
+                {/* ============= Start of Courses ================= */}
+                <div className="courses-table">
+                    <h2>Attendance</h2>
+
+                    <table>
+                        <thead>
+                        <tr>
+                            <th>Serial No.</th>
+                            <th>Course Name</th>
+                            <th>View Details</th>
+                        </tr>
+                        </thead>
+
+                        <tbody>
+                        {
+                            courses.map((course, index) => {
+                                return (
+                                    <tr key={index}>
+                                        <td>{index + 1}</td>
+                                        <td>{course.name}</td>
+                                        <Link
+                                            href={"/dashboard-teacher/attendance/attendance-details"}
+                                            className="primary"
+                                        >
+                                            Open
+                                        </Link>
+                                    </tr>
+                                )
+                            })
+                        }
+                        {/*<tr>
+                            <td>01</td>
+                            <td>English</td>
+                            <Link
+                                href={"/dashboard-teacher/attendance/attendance-details"}
+                                className="primary"
+                            >
+                                Open
+                            </Link>
+                        </tr>*/}
+                        </tbody>
+                    </table>
+                </div>
+                {/* ============= End of Courses  ================== */}
+            </main>
+            {/*=============== End Of Main  ==================*/}
+        </LayoutTeacher>
+    );
 };
 
 export default Attendance;
