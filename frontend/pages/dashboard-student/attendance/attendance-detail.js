b899b587ca07011190952b0488811a339b7844af
MuhammadMubeen1012 <muhammadmubeen1012@gmail.com>
2023-05-16 18:44:58 +0500

    Integration student/attendance

--- frontend/pages/dashboard-student/attendance/attendance-detail.js
+++ frontend/pages/dashboard-student/attendance/attendance-detail.js
@@ -1,59 +1,92 @@
-import React, {useState} from 'react';
-import {Button} from "react-bootstrap";
+import React, { useEffect, useState } from "react";
+import { Button } from "react-bootstrap";
 import LayoutStudent from "../../../components/Dashboard/Layout/LayoutStudent";
+import { useRouter } from "next/router";
+import Router from "next/router";
+import { getAttendance } from "../controllers/attendanceController";
+import { getStudent } from "../controllers/commonControllers";
 
 const AttendanceDetail = () => {
+  const [attendance, setAttendance] = useState([]);
+  const [loadAttendance, setLoadAttendance] = useState(false);
+  const [attendanceList, setAttendanceList] = useState([]);
+  const [courseID, setCourseID] = useState();
+  const [userID, setUserID] = useState();
 
+  const router = useRouter();
 
+  useEffect(() => {
+    if (router.isReady) {
+      setCourseID(router.query.courseId);
+      getStudent().then((res) => {
+        // console.log(res.student._id);
+        setUserID(res.student._id);
+        // console.log("Logged in Student", res);
+      });
+      //   const { _id } = JSON.parse(localStorage.getItem("user"));
+      //   setUserID(_id);
+    }
+  });
 
-    return (
-        <LayoutStudent>
+  useEffect(() => {
+    if (courseID && userID) {
+      getAttendance(courseID, userID).then((res) => {
+        // console.log(res);
+        setAttendance(res.attendance);
+      });
+    }
+  }, []);
 
-            {/*=============== Start of main ================= */}
-            <main>
-                <h1>Attendance</h1>
+  useEffect(() => {
+    if (attendance) {
+      setLoadAttendance(true);
+      console.log("Att", attendance);
+    }
+  });
 
-                {/* ============= Start of Courses ================= */}
-                <div className="courses-table">
+  return (
+    <LayoutStudent>
+      {/*=============== Start of main ================= */}
+      <main>
+        <h1>Attendance</h1>
 
+        {/* ============= Start of Courses ================= */}
+        <div className="courses-table">
+          <br />
+          {/*========================= Attendance Table ========================== */}
+          <table>
+            <thead>
+              <tr>
+                <th>Serial No.</th>
+                <th>Date</th>
+                <th>Status</th>
+              </tr>
+            </thead>
 
-                    <br/>
-                    {/*========================= Attendance Table ========================== */}
-                    <table>
-                        <thead>
-                            <tr>
-                                <th>Serial No.</th>
-                                <th>Date</th>
-                                <th>Status</th>
-                            </tr>
-                        </thead>
-
-                        <tbody>
-                            <tr>
-                                <td>1</td>
-                                <td>1-Apr-2023</td>
-                                <td style={{color: "green", fontWeight: "bold" }}>Present</td>
-                            </tr>
-
-                            <tr>
-                                <td>2</td>
-                                <td>2-Apr-2023</td>
-                                <td style={{color: "red", fontWeight: "bold" }}>Absent</td>
-                            </tr>
-
-                        </tbody>
-                    </table>
-
-                </div>
-                {/* ============= End of Courses  ================== */}
-
-            </main>
-            {/*=============== End Of Main  ==================*/}
-
-
-
-        </LayoutStudent>
-    );
+            <tbody>
+              {loadAttendance && attendance
+                ? attendance.map((list, index) => (
+                    <tr>
+                      <td>{index + 1}</td>
+                      <td>
+                        {new Date(
+                          Date.parse(list.createdAt)
+                        ).toLocaleDateString()}
+                      </td>
+                      <td style={{ color: "green", fontWeight: "bold" }}>
+                        {list.attendanceList[0].status}
+                      </td>
+                    </tr>
+                  ))
+                : ""}
+            </tbody>
+          </table>
+        </div>
+        {/* ============= End of Courses  ================== */}
+      </main>
+      {/*=============== End Of Main  ==================*/}
+    </LayoutStudent>
+  );
 };
 
 export default AttendanceDetail;
--- frontend/pages/dashboard-student/attendance/index.js
+++ frontend/pages/dashboard-student/attendance/index.js
@@ -1,48 +1,64 @@
-import React from 'react';
+import React, { useEffect, useState } from "react";
 import Link from "next/link";
 import LayoutStudent from "../../../components/Dashboard/Layout/LayoutStudent";
+import { getCourses } from "../controllers/commonControllers";
 
 const Attendance = () => {
-    return (
-        <LayoutStudent>
-            {/*=============== Start of main ================= */}
-            <main>
-                <h1>Attendance</h1>
+  const [courses, setCourses] = useState([]);
 
+  useEffect(() => {
+    getCourses().then((res) => {
+      console.log(res.courses);
+      setCourses(res.courses);
+    });
+  }, []);
 
-                {/* ============= Start of Courses ================= */}
-                <div className="courses-table">
+  return (
+    <LayoutStudent>
+      {/*=============== Start of main ================= */}
+      <main>
+        <h1>Attendance</h1>
 
-                    <h2>Courses</h2>
+        {/* ============= Start of Courses ================= */}
+        <div className="courses-table">
+          <h2>Courses</h2>
 
-                    <table>
-                        <thead>
-                        <tr>
-                            <th>Serial No.</th>
-                            <th>Course Name</th>
-                            <th>View Details</th>
+          <table>
+            <thead>
+              <tr>
+                <th>Serial No.</th>
+                <th>Course Name</th>
+                <th>View Details</th>
+              </tr>
+            </thead>
 
-                        </tr>
-                        </thead>
-
-                        <tbody>
-                        <tr>
-                            <td>01</td>
-                            <td>English</td>
-                            <Link href={"/dashboard-student/attendance/attendance-detail"} className="primary">
-                                Open
-                            </Link>
-                        </tr>
-                        </tbody>
-                    </table>
-
-                </div>
-                {/* ============= End of Courses  ================== */}
-
-            </main>
-            {/*=============== End Of Main  ==================*/}
-        </LayoutStudent>
-    );
+            <tbody>
+              {courses.map((course, idx) => (
+                <tr>
+                  <td>{idx + 1}</td>
+                  <td>{course.name}</td>
+                  <Link
+                    className="primary"
+                    href={{
+                      pathname: `/dashboard-student/attendance/attendance-detail`,
+                      query: {
+                        courseId: course._id,
+                        courseName: course.name,
+                      },
+                    }}
+                  >
+                    Open
+                  </Link>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
+        {/* ============= End of Courses  ================== */}
+      </main>
+      {/*=============== End Of Main  ==================*/}
+    </LayoutStudent>
+  );
 };
 
 export default Attendance;
